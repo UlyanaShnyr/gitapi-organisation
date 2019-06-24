6122dbbb7d3c6594f37aec84f9759897ba14ab42
unknown <ulyana250188@gmail.com>
2019-06-24 19:49:45 +0300

    Updates

---
+++ precache-manifest.febe8d79e3a8d99504c14be5b3bdb68e.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "b02a7ac0550b9db5bf6cc18df593f4b4",
+    "url": "/gitapi-organisation/index.html"
+  },
+  {
+    "revision": "2035d31a9d1fe622438a",
+    "url": "/gitapi-organisation/static/css/2.0624914a.chunk.css"
+  },
+  {
+    "revision": "82661fb71bba4e38bdcf",
+    "url": "/gitapi-organisation/static/css/main.d552b6d4.chunk.css"
+  },
+  {
+    "revision": "2035d31a9d1fe622438a",
+    "url": "/gitapi-organisation/static/js/2.2df63ba7.chunk.js"
+  },
+  {
+    "revision": "82661fb71bba4e38bdcf",
+    "url": "/gitapi-organisation/static/js/main.3f1f46ec.chunk.js"
+  },
+  {
+    "revision": "8c332f20b78e3519c9f4",
+    "url": "/gitapi-organisation/static/js/runtime~main.e3d48e9e.js"
+  }
+]);
